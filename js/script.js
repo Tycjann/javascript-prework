1901dcbca253970e10b3ba22f21e963d84a95cc2
Mirosław Gucwa <mgucwa@gmail.com>
2022-03-02 23:19:46 +0100

    Add: Player move

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 let randomNumber = Math.floor(Math.random() * 3 + 1);
-let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Wylosowana liczba to: ' + randomNumber);
-console.log('Gracz wpisał: ' + playerInput);
 
 let computerMove = 'nieznany ruch';
-let playerMove = 'nieznany ruch';
 
 if (randomNumber == 1) {
     computerMove = 'kamień';
@@ -17,8 +13,19 @@ else if (randomNumber == 3) {
     computerMove = 'nożyce';
 }
 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
 if (playerInput == 1) {
     playerMove = 'kamień';
+}
+else if (playerInput == 2) {
+    playerMove = 'papier';
+}
+else if (playerInput == 3) {
+    playerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
